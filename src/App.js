8f5d3d78a133dbf5f858fa69b77eddb2503cbb2b
Tenzin <tenthinten@gmail.com>
2022-01-06 21:28:25 +0530

    adding menu icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router,Switch,Route } from 'react-router-dom' 
+import {BrowserRouter as Router,Switch,Route } from 'react-router-dom'
+
 function App() {
   return (
     <>
     <Router>
      <Navbar/>
      <Switch>
-       <Route path='/' excat />
+       <Route path='/' exact />
      </Switch>
     </Router>
     
