3c1a4686c1cf28452e43dd840e1a772ab8448161
Tenzin <tenthinten@gmail.com>
2022-01-07 20:53:39 +0530

    create signup/prodcuts/service pages

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -23,19 +23,19 @@ function Cards() {
                     </ul>
                     <ul className="cards__items">
                         <CardItem 
-                            src="images/img-1.jpg"
+                            src="images/img-3.jpg"
                             text="Explore the hidden waterfall deep inside the amazon jungle"
                             label="Adventure"
                             path='./services'
                         />
                         <CardItem 
-                            src="images/img-5.jpg"
+                            src="images/img-4.jpg"
                             text="Travel through the island of Bali in a private Cruise"
                             label="Luxury"
                             path='./services'
                         />
                         <CardItem 
-                            src="images/img-4.jpg"
+                            src="images/img-8.jpg"
                             text="Travel through the island of Bali in a private Cruise"
                             label="Luxury"
                             path='./services'
