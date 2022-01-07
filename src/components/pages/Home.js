735099e3f5419a16693203caad80dcd4af7bff8a
Tenzin <tenthinten@gmail.com>
2022-01-07 10:50:06 +0530

    background video added

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import '../../App.css'
+import React from 'react'
+import HeroSection from '../HeroSection'
+
+
+function Home () {
+    return (
+        <>
+            <HeroSection />
+        </>
+    )
+}
+
+export default Home;
