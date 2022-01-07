3a797a5a872a842c739e5e7caaf5f12990492b27
Tenzin <tenthinten@gmail.com>
2022-01-07 21:51:26 +0530

    add footer icons and descriptions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { Button } from './Button';
+import './Footer.css'
 
 function Footer() {
     return (
@@ -22,6 +24,92 @@ function Footer() {
                         />
                         <Button buttonStyle='btn--outline'>Subscribe</Button>
                     </form>
+                </div>
+            </section>
+            <div className="footer-links">
+                <div className="footer-link-wrapper">
+                    <div className="footer-link-items">
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it Wroks</Link>
+                        <Link to='/'>Testimonial</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Services</Link>
+                    </div>
+                    <div className="footer-link-items">
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it Wroks</Link>
+                        <Link to='/'>Testimonial</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Services</Link>
+                    </div>
+                </div>
+                <div className="footer-link-wrapper">
+                    <div className="footer-link-items">
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it Wroks</Link>
+                        <Link to='/'>Testimonial</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Services</Link>
+                    </div>
+                    <div className="footer-link-items">
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it Wroks</Link>
+                        <Link to='/'>Testimonial</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Services</Link>
+                    </div>
+                </div>
+            </div>
+            <section className="social-media">
+                <div className="social-media-wrap">
+                    <div className="footer-logo">
+                        <Link className="social-logo">
+                            TRVL <i className="fab fa-typo3"/>
+                        </Link>
+                    </div>
+                    <small className="website-rights">TRVL 2020</small>
+                    <div className="social-icons">
+                        <Link className="social-icon-link facebook"
+                        to='/'
+                        target='_blank'
+                        aria-label='Facebook'
+                        >
+                            <i className="fab fa-facebook-f"></i>
+                        </Link>
+                        <Link className="social-icon-link instagram"
+                        to='/'
+                        target='_blank'
+                        aria-label='Instagram'
+                        >
+                            <i className="fab fa-instagram"></i>
+                        </Link>
+                        <Link className="social-icon-link youtube"
+                        to='/'
+                        target='_blank'
+                        aria-label='Youtube'
+                        >
+                            <i className="fab fa-youtube"></i>
+                        </Link>
+                        <Link className="social-icon-link twitter"
+                        to='/'
+                        target='_blank'
+                        aria-label='Twitter'
+                        >
+                            <i className="fab fa-twitter"></i>
+                        </Link>
+                        <Link className="social-icon-link linkedin"
+                        to='/'
+                        target='_blank'
+                        aria-label='Linkedin'
+                        >
+                            <i className="fab fa-linkedin"></i>
+                        </Link>
+
+                    </div>
                 </div>
             </section>
         </div>
